aaca9a7234aecab2fd280e6c3f7875c8306a98df
jayzjzhang <jayzjzhang@tencent.com>
2023-03-13 16:07:17 +0800

    返回结果

--- src/components/ValidateInput.tsx
+++ src/components/ValidateInput.tsx
@@ -50,7 +50,9 @@ export default defineComponent({
           return passed
         })
         inputRef.error = !allPassed
+        return allPassed
       }
+      return true
     }
 
     return () => {
